8d385837ce8f6bda5ac88086e5b677843043d54a
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-06-09 16:18:28 -0700

    done

--- src/14_cal.py
+++ src/14_cal.py
@@ -2,9 +2,8 @@
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
 Write a program that accepts user input of the form
-  `14_cal.py [month] [year]`
+  `14_cal.py month [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
@@ -17,16 +16,24 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
-optional, as this is a common convention in documentation.
-
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
-it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+today = datetime.today()
+
+print(today.year())
+print(today.month())
+def printMonth(x, y):
+    print(calendar.month(x, y))
+
+if len(sys.argv) == 2:
+    printMonth(today.year(), sys.argv[1])
+elif len(sys.argv) == 3:
+    printMonth(sys.argv[2], sys.argv[1])
+elif len(sys.argv) == 1:
+    printMonth(today.year(), today.month())
+else:
+    print("please include year and month in arguments")
